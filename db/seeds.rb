a1fe98a4144ec8728497776cc577f92bf19abc40
= <ninobessa@gmail.com>
2022-12-13 00:23:38 -0300

    create seed to import users list from API

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "Getting list of users..."
+url = "https://randomuser.me/api?page=1&results=30&inc=nat,gender,name,email,Picture&nat=br&seed=giga"
+json = JSON.load(URI.open(url))
+users = json["results"].map { |v| OpenStruct.new(v) }
+
+puts "Creating #{users.size} users..."
+users.each do |user|
+  user_name = [user.name["first"], user.name["last"]].join(' ')
+  user_picture = URI.parse(user.picture["large"]).open
+
+  User.find_or_create_by(email: user.email, name: user_name) do |u|
+    u.gender = user.gender
+    u.picture.attach(io: user_picture, filename: "#{user_name.parameterize}_picture.jpg")
+  end
+end
